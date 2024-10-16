1579d44ab0e000ea8cd2991b5fdb58572cd11ffe
Vanillaaaa <vanillaaaajp@gmail.com>
2024-10-16 15:01:36 +0800

    feat: init

---
+++ ffxiv/update.py
@@ -0,0 +1,63 @@
+import json
+import requests
+import time
+from urllib.parse import urlparse
+
+def update_time():
+    with open('ffxiv/dalamudrepo.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    current_timestamp = int(time.time())
+
+    for plugin in data:
+        plugin["LastUpdate"] = current_timestamp
+
+    with open('ffxiv/dalamudrepo.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def get_download_count(repo_owner, repo_name) -> int:
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    try:
+        total_download_count = 0
+        for release in data:
+            for asset in release["assets"]:
+                download_count = asset["download_count"]
+                total_download_count += download_count
+    finally:
+        return total_download_count
+
+def update_download_count(repo_owner):
+    with open('ffxiv/dalamudrepo.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    for plugin in data:
+        repoUrl = plugin["RepoUrl"]
+        parsed_url = urlparse(repoUrl)
+        path = parsed_url.path
+        last_part = path.split('/')[-1]
+        plugin["DownloadCount"] = get_download_count(repo_owner, last_part)
+
+    with open('ffxiv/dalamudrepo.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def reorder_json():
+    with open('ffxiv/dalamudrepo.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    key_order = [
+    "Author", "Name", "InternalName", "DalamudApiLevel", "Punchline", "Description",
+    "Tags", "CategoryTags", "RepoUrl", "IconUrl", "AssemblyVersion", "ApplicableVersion", "IsHide", 
+    "IsTestingExclusive", "DownloadCount", "DownloadLinkInstall", 
+    "DownloadLinkTesting", "DownloadLinkUpdate", "LastUpdate"
+    ]
+    for entry in data:
+        entry_reordered = {key: entry[key] for key in key_order if key in entry}
+        entry.clear()
+        entry.update(entry_reordered)
+    with open('ffxiv/dalamudrepo.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+update_time()
+update_download_count("KirisameVanilla")
+reorder_json()
---
+++ maimai/blog/星星B培养计划.md
@@ -0,0 +1,39 @@
+## 夹键
+
+拍完星星头，这只手需要拍其他的note，然后才滑星星
+
+标准谱 红 destr0yer LV11+ BV1HS4y117RY 
+
+打这个下意识的，拍完星星头，手离开屏幕离开外键，处理其他配置（在这里是慢三连交互）
+
+处理结束后，等星星开始再跟着一起滑
+
+## 一笔画
+
+标准谱 白 逐渐变快 LV12 BV1894y1o7wX
+
+https://www.bilibili.com/video/BV1894y1o7wX
+
+这个可以跟着一起唱，容易把握节奏，变速一笔画
+
+牢记一笔画
+
+**千万不要换手**
+
+## 错位入门
+
+标准谱 紫 TicTac LV11 BV1cq4y1T72c
+
+https://www.bilibili.com/video/BV1cq4y1T72c
+
+先看谱，再上机。先看谱，再上机。
+
+所有星星的启动拍，都是下一个正音，因此就算没有启动TAP，也要当成双压处理
+
+## 拍滑初级
+
+标准谱 紫 妄想感伤代偿联盟 LV10+ BV1LL4y1e7qb
+
+https://www.bilibili.com/video/BV1LL4y1e7qb
+
+拍滑，无需多言
